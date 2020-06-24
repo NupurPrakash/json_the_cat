ce2bfdcf973fc06cb89c3d511d7b5d9d5cd9a5c6
NupurPrakash <prakashnupurprakash@gmail.com>
2020-06-24 19:35:17 +0000

    after refactoring

---
+++ index.js
@@ -0,0 +1,12 @@
+const fetchBreedDescription = require('./breedFetcher');
+const breedName = process.argv[2];
+fetchBreedDescription(breedName,(error,desc) => {
+  if (error) {
+    console.log('Fetch error details', error);
+  }
+  console.log(desc);
+  
+});
+
+
+  
